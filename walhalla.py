3a06d3c4b123176bd6f6962a232698576f94eff2
pierelucas <pierelucas@keemail.me>
2019-10-09 16:35:05 +0200

    git init

---
+++ walhalla.py
@@ -0,0 +1,17 @@
+class Walhalla():
+
+    def __init__(self):
+        pass
+
+    def input(self):
+        pass
+
+    def out(self):
+        pass
+
+    def run(self):
+        pass
+
+# TO BE CONTINUED ...
+walhalla = Walhalla()
+walhalla.run()
