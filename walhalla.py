72b91e9e2580afe1774c918a0a41cef943d2beae
pierelucas <pierelucas@keemail.me>
2019-10-20 23:09:55 +0200

    walhalla

--- walhalla.py
+++ walhalla.py
@@ -4,14 +4,18 @@
 import os, sys, time, subprocess
 import socket
 from colorama import Fore, Style
+from argparse import ArgumentParser
 
 class Dos():
 
-    def tcp_flood(self):
-        pass
+    def tcp_flood(self, ip):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((ip, 80))
+            pass
 
-    def udp_flood(self):
-        pass
+    def udp_flood(self, ip):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+            sock.sendto(data, (ip, 80))
 
 class Controller(Dos):
 
@@ -42,8 +46,31 @@ class Controller(Dos):
         self.port = None
         self.num_req = None
 
-    def input(self):
-        pass
+    def arguments(self):
+        parser = ArgumentParser(description=self.banner_txt)
+        parser.add_argument("-t", "--target", dest="target_addr")
+        parser.add_argument("-m", "--mode", dest="dos_mode")
+        args = parser.parse_args()
+        _true = self.check_args(args)
+        if _true:
+            return args.target_addr, args.dos_mode
+
+    def check_args(self, args):
+        if args.target_addr and args.dos_mode:
+            return True
+        elif args.target_addr:
+            print("Please define dos mode [tcp/udp]")
+            sys.exit(0)
+        elif arg.dos_mode:
+            print("Please define target adress")
+            sys.exit(0)
+        else:
+            print("Use -h or --help for futher information")
+            sys.exit(0)
+
+    def get_fqdm(self, target_addr):
+        target_ip = socket.gethostbyname(target_addr)
+        return target_ip
 
     def out(self):
         print(Fore.CYAN + self.banner_txt)
@@ -53,9 +80,9 @@ class Controller(Dos):
         pass
 
     def run(self):
-        self.out()
-        self.input()
+        target_addr, dos_mode = self.arguments()
+        target_ip = self.get_fqdm(target_addr)
 
 if __name__ == "__main__":
-    walhalla = Walhalla()
-    walhalla.run()
+    cc = Controller()
+    cc.run()
