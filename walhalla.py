827a07ae4dbc8d9ed0bfb87abcd2a946ccd2ea94
pierelucas <pierelucas@keemail.me>
2019-10-21 18:51:17 +0200

    walhalla

--- walhalla.py
+++ walhalla.py
@@ -66,7 +66,7 @@ class Controller(Dos):
         self.time_hm = time.strftime("%H:%M")
 
         # Banner
-        self.version = "v1.0"
+        self.version = "v1.1"
         self.banner_txt = time.strftime("""
                          _       __      ____          ____     
                         | |     / /___ _/ / /_  ____ _/ / /___ _
