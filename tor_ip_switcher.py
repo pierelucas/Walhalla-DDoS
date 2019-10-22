e9ffcd9ede2df074e1c8850c38efbcedefdcc6e3
pierelucas <pierelucas@keemail.me>
2019-10-22 13:16:24 +0200

    tor ip switcher

---
+++ tor_ip_switcher.py
@@ -0,0 +1,12 @@
+# Author: PiereLucas(Julian Huch)
+# Creation: 22.10.2019
+# Last update: 22.10.2019w
+
+"""
+Tor Ip Switcher - We have to use a different exit Node for ervery thread to make das DoS attack look like a
+DDoS attack.
+"""
+
+# This module is under dev.
+# Don't use
+
