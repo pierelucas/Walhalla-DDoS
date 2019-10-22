d3558b84276a6b2318b8254856b1152e1d9a4084
pierelucas <pierelucas@keemail.me>
2019-10-22 14:13:59 +0200

    tor ip switcher

--- modules/tor_ip_switcher.py
+++ modules/tor_ip_switcher.py
@@ -10,5 +10,37 @@ DDoS attack.
 # This module is under dev.
 # Don't use
 
-# modules
+# Dist Modules
 import stem
+import os
+import sys
+# Site Modules
+from modules.colorama import Fore
+
+# Colors
+GREEN = Fore.GREEN
+RED = Fore.RED
+CYAN = Fore.CYAN
+RESET = Fore.RESET
+
+
+def check_for_file():
+    global tor_pass
+    try:
+        if os.path.isfile("../tor_pass.txt"):
+            with open("../tor_pass.txt", 'rt') as f:
+                tor_pass = f.read()
+                if tor_pass == "":
+                    print("tor_pass file is empty")
+                    sys.exit(0)
+        else:
+            tor_pass = str(input("Tor Control Password » "))
+            with open("../tor_pass.txt", 'wt') as f:
+                f.write(tor_pass)
+    except PermissionError as ex:
+        print("no read/write permission :", ex)
+        sys.exit(0)
+
+
+check_for_file()
+print(tor_pass)
