178f1d9c075db1122255a425523c167f7aef82da
pierelucas <pierelucas@keemail.me>
2019-10-23 13:51:28 +0200

    adding syn flood

--- walhalla.py
+++ walhalla.py
@@ -115,7 +115,7 @@ class Controller(Dos):
         self.time_hm = time.strftime("%H:%M")
 
         # Banner
-        self.version = "v1.3"
+        self.version = "v1.4"
         self.banner_txt = time.strftime("""
                          _       __      ____          ____     
                         | |     / /___ _/ / /_  ____ _/ / /___ _
